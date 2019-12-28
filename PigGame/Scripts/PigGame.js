c7a622655b75c5129ca627bc9d936dde157cff12
Sweta Chauhan <sweta.chauhan14@gmail.com>
2019-12-28 04:05:10 -0600

    Add project files.

---
+++ PigGame/Scripts/PigGame.js
@@ -0,0 +1,12 @@
+﻿var score, roundScore, activePlayer;
+//alert()
+document.getElementById("btnRoll").addEventListener("click", rollDice);
+
+function rollDice() {
+    var numText = ["one", "two", "three", "four", "five", "six"];
+    var ranDigit = Math.floor(Math.random() * 6);
+   // alert(ranDigit);
+    document.getElementById('awsDice').style.display = "block";
+    document.getElementById('awsDice').className = `fas fa-dice-${numText[ranDigit]}`;
+
+}
